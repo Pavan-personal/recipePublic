96c816796b46122406c2dda910d70a4a6f418707
Pavan Kumar Anupoju <145895808+Pavan-personal@users.noreply.github.com>
2024-06-13 17:36:34 +0530

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTVAgNbaHvgVrPg1gw15FdUTLRKhuYWEc",
-  authDomain: "my-assignment-auth.firebaseapp.com",
-  projectId: "my-assignment-auth",
-  storageBucket: "my-assignment-auth.appspot.com",
-  messagingSenderId: "627966424638",
-  appId: "1:627966424638:web:24f7f48e45f790ad20836f",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
